2666085b755412983365772d0e268d6f73e55d3a
Melanie <melanienps@gmail.com>
2020-11-07 10:47:57 +0800

    Fix merge conflict

---
+++ src/main/java/seedu/address/model/ReadOnlyWordBank.java
@@ -0,0 +1,25 @@
+package seedu.address.model;
+
+import javafx.collections.ObservableList;
+import seedu.address.model.deck.Deck;
+
+
+/**
+ * Unmodifiable view of a word bank
+ */
+public interface ReadOnlyWordBank {
+
+    /*
+     *
+     * Returns an unmodifiable view of the entries list.
+     * This list will not contain any duplicate entries.
+     */
+    /*ObservableList<Entry> getEntryList();*/
+
+    /**
+     * Returns an unmodifiable view of the deck list.
+     * This list will not contain any duplicate decks.
+     */
+    ObservableList<Deck> getDeckList();
+
+}
